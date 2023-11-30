d5b61e62e773878cf55fb94ed357aff539b16b49
loqman <59010033+loqmanali@users.noreply.github.com>
2023-11-30 11:46:29 +0300

    Add files via upload

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -4,9 +4,9 @@ const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 
 const RESOURCES = {"version.json": "a9430ba0446b9edb03683875ecf6a8f8",
-"index.html": "70cad1584df18f75a330d0a2086a2ff4",
-"/": "70cad1584df18f75a330d0a2086a2ff4",
-"main.dart.js": "4f9098dc637763b2c59c7aa6df1e94e0",
+"index.html": "7e3d34b3ccd2c1b6ce4d926312bfdd46",
+"/": "7e3d34b3ccd2c1b6ce4d926312bfdd46",
+"main.dart.js": "13695bc2da59b95c9426f7c4e7386e8b",
 "flutter.js": "6fef97aeca90b426343ba6c5c9dc5d4a",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
